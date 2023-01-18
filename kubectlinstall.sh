d85d965a255ad169c081cc35379ccfee018e45ee
muraliorion <121975512+muraliorion@users.noreply.github.com>
2023-01-17 18:49:26 -0600

    Update kubectlinstall.sh

--- kubectlinstall.sh
+++ kubectlinstall.sh
@@ -1,5 +1,4 @@
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.24.7/2022-10-31/bin/linux/amd64/kubectl
-chmod +x ./kubectl
+curl -O curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.23.13/2022-10-31/bin/linux/amd64/kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
 echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 kubectl version --short --client

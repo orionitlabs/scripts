16523855ec39cd68305f91d911c68211b671de8e
vedacodes <99849936+vedacodes@users.noreply.github.com>
2023-03-29 16:46:25 -0500

    Update kubectlinstall.sh

--- kubectlinstall.sh
+++ kubectlinstall.sh
@@ -1,4 +1,5 @@
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.23.13/2022-10-31/bin/linux/amd64/kubectl
+chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
 echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
 kubectl version --short --client
